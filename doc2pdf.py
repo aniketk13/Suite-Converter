4ccf65aaefdad54e25cc71601395abf9b2636ea0
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2021-09-08 16:24:53 +0530

    cleaned code part-1

--- doc2pdf.py
+++ doc2pdf.py
@@ -1,9 +1,9 @@
 from docx2pdf import convert  # Conversion of document to pdf module
 import pyttsx3  # Conversion of Text to Speech
-import config
+import config  # Importing input and output directory paths
+
+
 # Initialization of Audio Engine
-
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -18,15 +18,19 @@ engine.setProperty('voice', voices[1].id)
 # Integer speech rate in words per minute, can be increased or decreased to change speed of speech
 engine.setProperty('rate', 200)
 
+print("Converting document to PDF".center(400))
+speak("Converting document to PDF")
+
 print("Enter the name of file without extension:")
 speak("Enter the name of file without extension")
-name = input()
-# docfile = r"C:\Users\Pranav\OneDrive\Desktop\Suite-Converter\Input Directory\\"+name+".docx"
-docfile = (config.inputpath+name+".docx")
-# address = r"C:\Users\Pranav\OneDrive\Desktop\Suite-Converter\Output Directory\\" + \
-#     name+"-converted.pdf"
-address = config.outputpath+name+"-converted.pdf"
+filename = input()
 
-convert(docfile, address)
+inputfile = (config.inputpath+filename+".docx")  # Saving the input file path
 
-# print("Your file has been converted successfully".center(400))
+outputfile = config.outputpath+filename + \
+    "-converted.pdf"  # Saving the output file path
+
+convert(inputfile, outputfile)
+
+print("Your file has been converted successfully".center(400))
+speak("Your file has been converted successfully")
